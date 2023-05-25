4c551486e4fb12e455297371d447113ac326b752
galkin_vm <cheep666@gmail.com>
2023-05-25 07:54:36 +0400

    Start lesson spring boot

---
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary1/WebLibrary1Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.weblibrary1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLibrary1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLibrary1Application.class, args);
+    }
+
+}
